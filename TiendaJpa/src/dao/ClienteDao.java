da3dd4675100af2014f8dc1011ca95e76a99cae2
Daniel <danielcaos@ufps.edu.co>
2020-05-28 06:55:56 -0500

    jpa

---
+++ TiendaJpa/src/dao/ClienteDao.java
@@ -0,0 +1,10 @@
+package dao;
+
+import entities.Cliente;
+import util.Conexion;
+
+public class ClienteDao extends Conexion<Cliente> implements GenericDao<Cliente> {
+	public ClienteDao{
+		super(Cliente.class);
+	}
+}
